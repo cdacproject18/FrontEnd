4e94a14c05f6997619c968518b5c0eb54aacdef4
Shirish Gupta <shirishgupta@protonmail.com>
2018-07-29 13:45:37 +0530

    added service to provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { SportsComponent } from './sports/sports.component';
-import { ConcertsComponent } from './concerts/concerts.component';
-import { LoginComponent } from './login/login.component';
-import { ReviewsComponent } from './reviews/reviews.component';
-import { AboutComponent } from './about/about.component';
-import { AppRoutingModule } from './/app-routing.module';
-
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SportsComponent } from "./sports/sports.component";
+import { ConcertsComponent } from "./concerts/concerts.component";
+import { LoginComponent } from "./login/login.component";
+import { ReviewsComponent } from "./reviews/reviews.component";
+import { AboutComponent } from "./about/about.component";
+import { AppRoutingModule } from ".//app-routing.module";
+import { ConcertsService } from "./concerts.service";
+import { SportsService } from "./sports.service";
 
 @NgModule({
   declarations: [
@@ -28,12 +29,8 @@ import { AppRoutingModule } from './/app-routing.module';
     ReviewsComponent,
     AboutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ConcertsService, SportsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
